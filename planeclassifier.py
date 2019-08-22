29d03e26d3182e2be647b162a7fc9c1c16bdd416
Arunav Gupta <centaurus.ab@gmail.com>
2019-08-21 17:43:43 -0700

    separate datasets

--- planeclassifier.py
+++ planeclassifier.py
@@ -16,28 +16,33 @@ test_split = 0.30 #proportion of data split away for use in test set
 
 className = ["Airbus", "Boeing"]
 
+# Import images from library
 airbus_dir = "./AirbusPics"
 boeing_dir = "./BoeingPics"
 imgs_airbus = ['./AirbusPics/{}'.format(i) for i in os.listdir(airbus_dir)]
 imgs_boeing = ['./BoeingPics/{}'.format(i) for i in os.listdir(boeing_dir)]
-print("Boeing pic count: " + str(len(imgs_boeing)))
 random.shuffle(imgs_airbus)
 random.shuffle(imgs_boeing)
+print("Total imgs: " + str(len(imgs_airbus) + len(imgs_boeing)))
 
-train_imgs = imgs_airbus[0:round(len(imgs_airbus)*(1.00-test_split))]
-train_imgs.append(imgs_boeing[0:round(len(imgs_boeing)*(1.00-test_split))])
+# Split into training and test sets
+train_imgs = imgs_airbus[0:round(len(imgs_airbus)*(1.00-test_split))] + imgs_boeing[0:round(len(imgs_boeing)*(1.00-test_split))]
 random.shuffle(train_imgs)
+test_imgs = imgs_airbus[round(len(imgs_airbus)*(1.00-test_split)): len(imgs_airbus)] + imgs_boeing[round(len(imgs_boeing)*(1.00-test_split)): len(imgs_boeing)]
+random.shuffle(test_imgs)
 
-test_imgs = imgs_airbus[]
-
-
+# Check to make sure images imported correctly
 img = ip.load_img(train_imgs[0])
-print(img.size)
+imgt = ip.load_img(test_imgs[0])
+print(str(img.size) + " \t " + str(imgt.size))
 img.show()
+print("Training pics: " + str(len(train_imgs)))
+print("Testing pics: " + str(len(test_imgs)))
+print("Full set: " + str(len(train_imgs) + len(test_imgs)))
 imgarray = ip.img_to_array(img)
-print(imgarray)
 print(imgarray.shape)
 
+# Augment dataset by altering each image and saving the copy
 datagen = ip.ImageDataGenerator(
     featurewise_center=True,
     featurewise_std_normalization=True,
